247ea03e5b0a1d8a432b18f2369357922dc8a487
wolfie0420 <jeffery.karasawa@pm.me>
2023-09-13 19:09:37 +0200

    putting weather command into cmd folder, added in imports

--- DiscordBotPython.py
+++ DiscordBotPython.py
@@ -43,44 +43,7 @@ async def on_ready():
 #async def on_message_edit(before, after):
 #    await before.channel.send(str(before.author) + " edited a message.\nBefore: " + before.content + "\nAfter: " + after.content)
 
-#using API to grab weather information
-    
-with open('api_key.json', 'r') as f:
-    api_key = json.load(f)
-    
-@client.command()
-@commands.cooldown(2, 1, commands.BucketType.default)
-async def weather (ctx, city: str, state : str = None, country : str = None):
-    
-    r = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city},{country}&units=metric&APPID={api_key['api_key']}")
-    json_data = r.json()
-    
-    weather = json_data['weather'][0]['main']
-    description = json_data['weather'][0]['description']
-    temp = json_data['main']['temp']
-    icon = "http://openweathermap.org/img/wn/" + json_data['weather'][0]['icon'] + "@2x.png"
-    
-    #print(weather, description, temp)
-#creating embed to send weather info to discord server
-    embed = discord.Embed(
-        title="Current Weather",
-        description=f"{city.upper()}",
-        color=discord.Color.blue()
 
-    )    
-    
-    embed.set_thumbnail(url=icon)
-    embed.add_field(name=weather, value=description, inline=False)
-    embed.add_field(name="Temperature", value=f"{temp}\u2103", inline=False)
-    
-    await ctx.send (embed=embed)
-#creating error handler for city and country
-@weather.error
-async def weather_error(ctx, error):
-    if isinstance(error, commands.CommandOnCooldown):
-        await ctx.send("This command is on cooldown, please wait a few seconds before inputting the command again")
-    elif isinstance(error, commands.CommandInvokeError):
-        await ctx.send("This is not a valid city or country, please try again.")
         
 
 
