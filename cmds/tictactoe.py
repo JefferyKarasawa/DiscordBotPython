fd833826ba758dac12bd0a42e25f1dd8a9441b38
wolfie0420 <jeffery.karasawa@pm.me>
2023-09-27 12:02:54 +0200

    created game board, and added logic to see who goes first with random.randint between player1 and player2

--- cmds/tictactoe.py
+++ cmds/tictactoe.py
@@ -29,9 +29,42 @@ async def tictactoe (ctx, p1: discord.Member, p2: discord.Member):
     if gameOver:
         global board
     #set the board into an array, input discord emoji of white_large_square
-        board = [":white_large_square:", ":white_large_square:", ":white_large_square:"
-                 ":white_large_square:", ":white_large_square:", ":white_large_square:"
+        board = [":white_large_square:", ":white_large_square:", ":white_large_square:",
+                 ":white_large_square:", ":white_large_square:", ":white_large_square:",
                  ":white_large_square:", ":white_large_square:", ":white_large_square:"]
+        
+        turn = ""
+        gameOver = False
+        count = 0
+        
+        player1 = p1
+        player2 = p2
+        
+    #print out the board to discord channel so the game can start
+
+        line = ""
+        #for loop for the length of board
+        for x in range (len(board)):
+            #if end of line then create new line
+            if x == 2 or x == 5 or x == 8:
+                line += " " + board[x]
+                await ctx.send(line)
+                line = ""
+            else:
+                line += " " + board[x]
+                
+        #lets determine who goes first
+        num = random.randint(1, 2)
+        if num == 1:
+            turn = player1
+            await ctx.send("<@" + str(player1.id) +">'s turn, you may start the game!")
+        elif num == 2:
+            turn = player2
+            await ctx.send("<@" + str(player2.id) +">'s turn, you may start the game!")
+            
+    #create an else statement just incase there is already a game in progress
+    else:
+        await ctx.send("A game is already in progress, please finish the current game in order to start a new game!")
     
     
 async def setup(client):
