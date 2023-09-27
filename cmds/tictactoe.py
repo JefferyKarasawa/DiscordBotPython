16364e642877dcf936d5705beaf7b21761ee9dce
wolfie0420 <jeffery.karasawa@pm.me>
2023-09-27 11:36:28 +0200

    putting in tictactoes players

--- cmds/tictactoe.py
+++ cmds/tictactoe.py
@@ -3,6 +3,23 @@ from discord.ext import commands
 import random
 
 
-player 1 = ""
-player 2 = ""
+player1 = ""
+player2 = ""
 turn = ""
+gameOver = True
+
+board = []
+
+
+@commands.group(
+    help="Initiates TicTacToe Game",
+    description="Command: tictactoe",
+    brief="Command to play tictactoe"
+)
+        
+async def tictactoe (ctx, p1: discord.Member, p2: discord.Member):
+    await ctx.send("Tictactoe game")
+    
+    
+async def setup(client):
+    client.add_command(tictactoe)
