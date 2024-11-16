f6c3c6eea44b0f909381651d1fb7885e3ddd6d58
wolfie0420 <jeffery.karasawa@pm.me>
2024-11-16 21:59:45 +0100

    added in xivapi hit with lodestone command

---
+++ cmds/lodestone.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands
+import requests
+
+
+
+
+XIVAPI_BASE_URL = 'https://xivapi.com/'
+
+XIVAPIKEY = '986957cd4c7247d68e97acf6030333b6688ab6642cba42c4b3701b1c4854db2a'
+
+@commands.group()
+async def lodestone(ctx):
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f"No, {ctx.subcommand_passed} does not belong to lodestone command group")
+        
+@lodestone.command()
+async def char (ctx, server: str, *, character_name: str):
+    params = {
+        'name': character_name,
+        'server': server,
+        'key': XIVAPIKEY
+    }
+    
+    response = requests.get(f'{XIVAPI_BASE_URL}character/search', params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        results = data.get('Results', [])
+
+        if results:
+            char_data = results[0]
+            await ctx.send(f"Found: {char_data['Name']} on {char_data['Server']}. Lodestone ID: {char_data['ID']}")
+        else:
+            await ctx.send("Character not found.")
+    else:
+        await ctx.send("Error retrieving data from the FFXIV API.")
+        
+async def setup(client):
+    client.add_command(lodestone)
